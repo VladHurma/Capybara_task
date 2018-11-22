49de3ccf06726f056ccd09e0fd70a8967425b66f
VladHurma <vladik_shyshko@mail.ru>
2018-11-22 15:23:44 +0300

    Get links from onliner

--- app.rb
+++ app.rb
@@ -1,9 +1,19 @@
-require "nokogiri"
-require "net/http"
-require "selenium-webdriver"
+require 'nokogiri'
+require 'rspec'
+require 'net/http'
+require 'selenium-webdriver'
+require 'capybara-screenshot/rspec'
+require 'pry'
+
+Capybara::Screenshot.autosave_on_failure = false
+
 
 uri = URI("https://www.onliner.by/")
 body = Net::HTTP.get(uri)
 
 document = Nokogiri::HTML(body)
-links = 
+links = document.css('li a')
+
+links.each do |link|
+	puts link.text, " #{link['href']} ", "\n"
+end
