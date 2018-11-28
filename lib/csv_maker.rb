0c9a66d917e09870a60f446f56e8842055292432
VladHurma <vladik_shyshko@mail.ru>
2018-11-28 12:30:11 +0300

    Rubocop restile

--- lib/csv_maker.rb
+++ lib/csv_maker.rb
@@ -1,13 +1,13 @@
+# frozen_string_literal: true
+
 require 'csv'
 
 module CSV_maker
-	def self.write_to_csv(content)
-		CSV.open("./news.csv", "wb") do |csv|
-			csv << ['Link', 'Header', 'Image', 'Body']
-			while !content.empty?
-				csv << content.shift(4)
-			end
-		end
-	puts "CSV created successfull!"
-	end
+  def self.write_to_csv(content)
+    CSV.open('./news.csv', 'wb') do |csv|
+      csv << %w[Link Header Image Body]
+      csv << content.shift(4) until content.empty?
+    end
+    puts 'CSV created successfull!'
+  end
 end
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,6 @@
-$LOAD_PATH.unshift(".")
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift('.')
 
 require 'selenium-webdriver'
 require 'nokogiri'
@@ -7,33 +9,34 @@ require 'csv_maker'
 require 'content_getter'
 
 class Capybara_scarper_for_onliner
-	def initialize
-		Selenium::WebDriver::Chrome.driver_path = '/home/hurma/prog/Capybara_task/chromedriver'
+  def initialize
+    Selenium::WebDriver::Chrome.driver_path = '/home/hurma/prog/Capybara_task/chromedriver'
 
-		Capybara.register_driver :selenium do |app|  
-		  Capybara::Selenium::Driver.new(app, browser: :chrome)
-		end
+    Capybara.register_driver :selenium do |app|
+      Capybara::Selenium::Driver.new(app, browser: :chrome)
+    end
 
-		Capybara.javascript_driver = :chrome
-		Capybara.configure do |config|  
-		  config.default_max_wait_time = 10 # seconds
-		  config.default_driver = :selenium
-		end
+    Capybara.javascript_driver = :chrome
+    Capybara.configure do |config|
+      config.default_max_wait_time = 10 # seconds
+      config.default_driver = :selenium
+    end
 
-		# Visit
-		@browser = Capybara.current_session
-		@driver = @browser.driver.browser
-		@content = []
-	end
-	def self.run
-		new.run
-	end
-	def run
-		content_get = Content_getter.new(@browser)
-		@content = content_get.run
-		CSV_maker.write_to_csv(@content)
-	end
+    # Visit
+    @browser = Capybara.current_session
+    @driver = @browser.driver.browser
+    @content = []
+  end
+
+  def self.run
+    new.run
+  end
+
+  def run
+    content_get = Content_getter.new(@browser)
+    @content = content_get.run
+    CSV_maker.write_to_csv(@content)
+  end
 end
-
 
 Capybara_scarper_for_onliner.run
